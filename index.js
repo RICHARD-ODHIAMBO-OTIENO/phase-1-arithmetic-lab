f4fdf3547c767167707830910554c48ec9063af8
Richard Odhiambo Otieno <kotienor1@gmail.com>
2024-03-27 13:57:36 +0300

    Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
-// Write your code here
+// Creation of variable num1 and num2
+let num1 = 31;
+let num2 = 2;
+
+// Creating a variable - multiply - and set it equals to the product out of multiplication of num1 and Num2
+
+let multiply = num1 * num2;
+
+// Generation of variable called random that integer > 0
+let random = Math.floor(Math.random() * 100) + 1;
+
+// Define variables num3 and num4 such that the remainder of num3 divided by num4 is 4
+let num3 = 36;
+let num4 = 8;
+
+// variable called mod set to an equation calculates the remainder of dividing num3 by num4
+let mod = num3 % num4;
+
+// Creation of variable called max that finds the highest number in a set
+let max = Math.max(3, 17, 11, 13, 20); // The value returned should be 20
